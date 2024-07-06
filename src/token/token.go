4c95f5281e2a766af0e32b2a0a84e56a215f526d
abdallahsafa <abdallah.safa@vanderbilt.edu>
2024-07-06 00:52:31 -0400

    First commit

---
+++ src/token/token.go
@@ -0,0 +1,30 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENTIFIER = "IDENTIFIER" // add, foobar, x, y, ...
+	INTEGER    = "INTEGER"    // 1343456
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LCURLY = "{"
+	RCURLY = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
