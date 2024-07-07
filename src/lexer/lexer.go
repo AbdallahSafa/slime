fa18dd41f9e90503646db2a86f13e296510139ff
abdallahsafa <abdallah.safa@vanderbilt.edu>
2024-07-07 02:34:26 -0400

    Lexer continued.

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -1,1 +1,40 @@
 package lexer
+
+import "go/token"
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) nextChar() token.Token {
+	var token token.Token
+	switch l.ch {
+	case '=':
+		token = newToken(token.ASSIGN, l.ch)
+	}
+	case '+':
+
+}
