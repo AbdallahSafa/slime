c333c418b5d0e5ea6e826f8ba070e1ce52db26aa
abdallahsafa <abdallah.safa@vanderbilt.edu>
2024-07-08 14:21:48 -0400

    AST continued. Struct for Let statements.

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "github.com/AbdallahSafa/slime/src/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -25,3 +27,23 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 }
+
+type Identifier struct { // var name
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+func (ls *LetStatement) statementNode() {}
